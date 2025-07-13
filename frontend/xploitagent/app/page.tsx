c5b667e4dd796b357bf9603a85b5864e80383d42
Aromal <aromals161@gmail.com>
2025-07-13 19:17:56 +0530

    removed some components

--- frontend/xploitagent/app/page.tsx
+++ frontend/xploitagent/app/page.tsx
@@ -1,35 +1,19 @@
-import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Chat from "./components/includes/Chat";
-import Footer from "./components/includes/Footer";
 import Navbar from "./components/includes/Navbar";
-import LatestNews from "./components/LatestNews";
-import PainPoint from "./components/PainPoint";
-import Solution from "./components/Solution";
 import Supporters from "./components/Supporters";
 import Threat from "./components/Threat";
-import VideoSection from "./components/VideoSection";
-import VidSec from "./components/VidSec";
 import WhoThisIsFor from "./components/WhoThisIsFor";
-import WhyChooseEyeShield from "./components/WhyChooseEyeShield";
 
 export default function Home() {
   return (
     <main className="">
+      <Chat />
       <Navbar />
       <Hero />
       <Supporters />
       <Threat />
       <WhoThisIsFor />
-      {/* <LatestNews /> */}
-      {/* <VidSec /> */}
-      {/*<VideoSection /> */}
-      {/* <WhyChooseEyeShield /> */}
-      {/* <PainPoint /> */}
-      {/* <Solution /> */}
-      {/* <Features /> */}
-      {/* <Footer /> */}
-      <Chat />
     </main>
   );
 }
--- frontend/xploitagent/app/xploitai/page.tsx
+++ frontend/xploitagent/app/xploitai/page.tsx
@@ -1,3 +1,18 @@
+// import { ArrowUpRight } from "lucide-react";
+// import Image from "next/image";
+
 export default function XploitAi() {
-  return <main>Aromal 🔥🔥🔥😎 write your ai code here</main>;
+  return (
+    <section className="flex items-center justify-center h-dvh">
+      <div>
+       
+      </div>
+    </section>
+  );
 }
+
+
+
+// <button className="bg-white text-neutral-900 px-2 py-2 rounded-lg inline-flex items-center justify-center gap-2">
+//   <code>Try XploitAi</code> <ArrowUpRight />
+// </button>;
