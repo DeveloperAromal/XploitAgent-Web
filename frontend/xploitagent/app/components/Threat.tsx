2f8d987673eb79ba1eae3a3045093433d0785303
Sabith <masterdevsabith@gmail.com>
2025-07-13 15:40:44 +0530

    updated the threat section

--- frontend/xploitagent/app/components/Threat.tsx
+++ frontend/xploitagent/app/components/Threat.tsx
@@ -1,10 +1,15 @@
+import SpotlightCard from "./ui/SpotlightCard";
+
 export default function Threat() {
   return (
     <section className="w-full px-0 pt-20 text-white">
       <div className="w-full">
         <div className="flex items-center justify-center pb-10 flex-wrap w-full">
           <div className="flex flex-wrap gap-10 w-full px-10">
-            <div className="border-2 border-neutral-50 rounded-3xl w-full lg:w-[calc(50%-1.25rem)] p-16 flex items-center justify-center bg-zinc-900/20">
+            <SpotlightCard
+              spotlightColor="rgba(7, 151, 88, 0.8)"
+              className="bg-emerald-800/20 border-2 border-neutral-50 rounded-3xl w-full lg:w-[calc(50%-1.25rem)] p-16 flex items-center justify-center "
+            >
               <div>
                 <h2 className="text-4xl text-center font-medium text-white pb-6">
                   <code> Threat Intelligence</code>{" "}
@@ -13,9 +18,12 @@ export default function Threat() {
                   <code># 1</code>
                 </h2>
               </div>
-            </div>
+            </SpotlightCard>
 
-            <div className="border-2 border-neutral-50 rounded-3xl w-full lg:w-[calc(50%-1.25rem)] p-16 flex items-center justify-center bg-zinc-900/40">
+            <SpotlightCard
+              spotlightColor="rgba(7, 151, 88, 0.8)"
+              className="bg-emerald-800/20 border-2 border-neutral-50 rounded-3xl w-full lg:w-[calc(50%-1.25rem)] p-16 flex items-center justify-center "
+            >
               <div>
                 <h2 className="text-4xl text-center font-medium text-white pb-6">
                   <code>Security rate</code>{" "}
@@ -24,7 +32,7 @@ export default function Threat() {
                   <code>70%</code>
                 </h2>
               </div>
-            </div>
+            </SpotlightCard>
           </div>
         </div>
       </div>
